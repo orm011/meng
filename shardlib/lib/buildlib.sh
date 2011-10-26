40a765acdc2b8d6f54164f13611163246428d21a
Oscar R Moll-Thomae <oscarm@twitter.com>
2011-10-26 16:36:40 -0700

    changed scripts to reflect new directory structure;

--- shardlib/lib/buildlib.sh
+++ shardlib/lib/buildlib.sh
@@ -1,15 +1,9 @@
-LIBFOLDER="lib"
 JUNITURL="http://cloud.github.com/downloads/KentBeck/"
-JUNITJAR="junit-4.10.jar"
+JUNITZIP="junit-4.10.zip"
 MATHURL="http://download.nextag.com/apache//commons/math/binaries/"
 MATHTAR="commons-math-2.2.tar.gz"
 
-
-if [[ ! -d $LIBFOLDER ]]; then
-mkdir $LIBFOLDER
-fi
-
-cd $LIBFOLDER
 curl $JUNITURL/$JUNITJAR -o $JUNITJAR
 curl $MATHURL/$MATHTAR -o $MATHTAR
+gunzip $JUNITZIP
 tar -xf $MATHTAR
