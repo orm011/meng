ebd21814bf30bb109f6193168a124648b5acf27b
Oscar R Moll-Thomae <oscarm@twitter.com>
2011-11-28 17:49:29 -0800

    satefy copy of scripts

--- shardlib/logs/plot.sh
+++ shardlib/logs/plot.sh
@@ -6,25 +6,63 @@
 # for log format: $37, now much less after removing prefix
 # argument is the number of rows written the last time
 
-OUTPUTFILE='intermediatedata.csv'
-METRICSMARKER='MetricsCollector'
-ENVMARKER='WorkloadParams'
+#NOTE: $2 is the number of lines of the log to read, ie number of vertices to put in the histogram or number of queries
+# $3 is maxX, $4 is maxY
+#example:
+#1    [main] DEBUG parameters.WorkloadParams  2011-11-26 23:35:07,787 SystemParameters edgePayload 50 workNodes 1 
+#2    [main] DEBUG parameters.WorkloadParams  2011-11-26 23:35:07,788 GraphParameters numberVertices 10 numberEdges 30 actualNumberEdges 30 maxDegree 10 actualMaxDegree 5 degreeSkew 1.0 
+#3    [main] DEBUG parameters.WorkloadParams  2011-11-26 23:35:07,789 WorkloadParams numberOfQueries 10 querySkew 0.0010 skewCorrelationWithGraph 0.0 percentEdge 0 percentVertex 100 percentIntersection 0 
+#3    [main] DEBUG simulated.Graph  2011-11-26 23:35:07,789 1
+#18   [main] DEBUG simulated.MetricsCollector  2011-11-26 23:35:07,804 148000
 
-#TODO: figure out way of including all plot details
-TITLE=$(cat example.log | grep $ENVMARKER | tail -n 1 | cut -d ' ' -f 15-30)
-echo $TITLE
+if [[ $# < 1 ]]
+then
+   echo "usage $0 --[latency|throughput|degree] NumberOfRowsToRead maxX maxY fileName"
+   exit 0
+fi
 
+MAXX=$3
+MAXY=$4
+FILE=$5
+
+SYSTEMMARKER='SystemParameters'
+GRAPHMARKER='GraphParameters'
+WORKLOADMARKER='WorkloadParameters'
+GRAPHDATA='simulated.Graph'
+QUERYDATA='simulated.MetricsCollector'
+
+#TODO: change octave to read from stdin for everything, remove intermediate file
+
+#NOTE: will have to change this according to Benchmark.java
+
+#TODO: make into function
+SYSTEM=$(cat $FILE | grep $SYSTEMMARKER | tail -n 1 | cut -d ' ' -f 6-)
+GRAPH=$(cat $FILE | grep $GRAPHMARKER | tail -n 1 | cut -d ' ' -f 6-)
+WORKLOAD=$(cat $FILE | grep $WORKLOADMARKER | tail -n 1 | cut -d ' ' -f 6-)
+
+echo $SYSTEM
+echo $GRAPH
+echo $WORKLOAD
 
 if [[ $1 == --latency ]]
 then
-cat example.log | grep $METRICSMARKER | tail -n $2  | awk '{print $7}'  > $OUTPUTFILE;
-octave --silent plot_latency.m $OUTPUTFILE "$TITLE"
+echo 'if'
+cat $FILE | grep $QUERYDATA | awk '{print $7}' |\
+octave --silent plot_latency.m "$SYSTEM" "$GRAPH" "$WORKLOAD" 'latency (nanos)' 'latency histogram' $MAXX $MAXY
+echo 'done'
 else
 if [[ $1 == --throughput ]]
 then
-tail -n $2 example.log | awk '{print $7}' | cut -c 8-16  > $OUTPUTFILE;
+#broken need to reimplement based on new format
+TITLE=$(cat $FILE | grep $WORKLOADMARKER | tail -n 1 | cut -d ' ' -f 15-30)
+#probably needs update
+tail -n $2 $FILE | awk '{print $7}' | cut -c 8-16  > $OUTPUTFILE;
 octave --silent plot_throughput.m $OUTPUTFILE "$TITLE"
 else
-echo "usage: $0 --[latency|throughput] [num. lines]"
+if [[ $1 == --degree ]] 
+then
+cat $FILE | grep $GRAPHDATA | awk '{print $7}' |\
+octave --silent plot_latency.m "$SYSTEM" "$GRAPH" "$WORKLOAD" 'degree' 'degree histogram' $MAXX $MAXY
+fi 
 fi
 fi
--- shardlib/logs/plot_latency.m
+++ shardlib/logs/plot_latency.m
@@ -1,22 +1,66 @@
 #!/usr/bin/octave
-
 args = argv();
 
-if length(args) < 2
-printf('usage: <script> datafile titlestring');
+%% process arguments
+max_argc = 7;
+opt_argc = 2;
+
+if length(args) < (max_argc - opt_argc)
+printf("usage: <script> systemparams graphparams workloadparams xlabel title maxx maxy\n");
+printf("NOTE: the first word of the title also goes to the output filename\n");
 quit;
 end
 
-filename = args{1};
-titlestring = args{2};
+system = args{1};
+graph = args{2};
+workload = args{3};
+xlabel = args{4}
+title = args{5};
 
-x = fscanf(fopen(filename), "%d");
+x = fscanf(stdin, "%d");
 x = x/1000;
 
-hist(x,100);
+if length(args) < max_argc
+maxfreq = length(x);
+maxnumber = max(x);
+maxy = maxfreq*0.5;
+maxx = 1.2*maxnumber;
+else
+maxx = sscanf(args{max_argc - 1}, "%d");
+maxy = sscanf(args{max_argc}, "%d");
+end
+
+%%
+%get quantiles before zooming in
+quantiles = [0, 50, 90, 99, 99.9, 99.99, 100]/100;
+stats = quantile(x, quantiles);
+
+quantilestring = sprintf("%.1f %.1f %.1f %.2f %.3f %.4f %.4f", quantiles);
+statstring = sprintf("%.1f %.1f %.1f %.1f %.1f %.1f %.1f", stats);
+
+disp(['quantiles: ', quantilestring])
+disp(['values: ', statstring]);
+
+%select only desired range
+x = x(find(x < maxx));
+hist(x,50);
 ylabel('frequency');
-xlabel('latency in musec');
-title(['latency distribution. n = ', sprintf("%d", length(x)), " ", titlestring], 'fontsize', 5);
+xlabel(xlabel);
+title(title, 'fontsize', 10);
+
+miny = -0.3*maxy; 
+minx = -0.1*maxx; 
+
+xlim([minx, maxx]);
+ylim([miny, maxy]);
+
+step = 0.025*(maxy - miny);
+text(0.5*maxx, 0.5*maxy + 0*step, quantilestring);
+text(0.5*maxx, 0.5*maxy + 1*step, statstring);
+
+show_parameters(system, graph, workload)
+graph_params = strsplit(graph, ' ', true);
+print([title{1}, graph_params{end}, '.pdf'], '-dpdf', '-landscape')
 
 sleep(0.01); 
 quit;
