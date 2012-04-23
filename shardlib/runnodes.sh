e21678652ba57a5601aea1632ef6b22e378e8098
Oscar <rimoll@gmail.com>
2012-04-23 18:43:40 -0400

    updated runnodes.sh

--- shardlib/runnodes.sh
+++ shardlib/runnodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CP='./bin/:./lib/commons-math-2.2.jar:./lib/commons-lang-2.6.jar:./lib/junit-4.10.jar:./lib/log4j-1.2.16.jar:./lib/slf4j-api-1.6.4.jar:./lib/slf4j-log4j12-1.6.4.jar:./lib/guava-11.0.1.jar'
+CP='./bin/:./lib/commons-math3-3.0.jar:./lib/commons-lang-2.6.jar:./lib/junit-4.10.jar:./lib/log4j-1.2.16.jar:./lib/slf4j-api-1.6.4.jar:./lib/slf4j-log4j12-1.6.4.jar:./lib/guava-11.0.1.jar'
 
 FLAGS='-Djava.security.policy=server.policy -Djava.rmi.server.codebase=file:./bin/'
 
